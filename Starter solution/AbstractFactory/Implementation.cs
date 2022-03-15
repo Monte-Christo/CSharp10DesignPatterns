54f0abef9da67cbf12aaab2a2367a2db30038106
Edgar Knapp <Edgar.Knapp@simcorp.com>
2022-03-15 17:59:54 +0100

    Abstract Factory impl done

--- Starter solution/AbstractFactory/Implementation.cs
+++ Starter solution/AbstractFactory/Implementation.cs
@@ -1,4 +1,52 @@
-﻿namespace AbstractFactory
+﻿namespace AbstractFactory;
+
+public interface IShoppingCartPurchaseFactory
 {
-   
+    IDiscountService CreateDiscountService();
+    IShippingCostService CreateShippingCostService();
 }
+
+public class FranceShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+{
+    public IDiscountService CreateDiscountService() => new FranceDiscountService();
+
+    public IShippingCostService CreateShippingCostService() => new FranceShippingCostService();
+}
+
+public class BelgiumShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+{
+    public IDiscountService CreateDiscountService() => new BelgiumDiscountService();
+
+    public IShippingCostService CreateShippingCostService() => new BelgiumShippingCostService();
+}
+
+public interface IShippingCostService
+{
+    decimal ShippingCost { get; }
+}
+
+public class FranceShippingCostService : IShippingCostService
+{
+    public decimal ShippingCost => 25.5m;
+}
+
+public class BelgiumShippingCostService : IShippingCostService
+{
+    public decimal ShippingCost => 20;
+}
+
+public interface IDiscountService
+{
+    int DiscountPercentage { get; }
+}
+
+public class FranceDiscountService : IDiscountService
+{
+    public int DiscountPercentage => 10;
+}
+
+public class BelgiumDiscountService : IDiscountService
+{
+    public int DiscountPercentage => 20;
+}
+
